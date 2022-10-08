f82e830a42ba177f4e1b40991019ceb4a542a5c1
Claudio Toledo <claudiotoledoit@gmail.com>
2022-10-07 23:28:26 -0300

    Insert change directory function

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,11 +1,13 @@
 variable "name" {
   description = "(Required) Specifies the name of the resources. Changing this forces a new resource to be created."
   type        = string
+  default = "talentpoolproject0001"
 }
 
 variable "location" {
   description = "(Required) Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
   type        = string
+  default = "eastus"
 }
 
 variable "tags" {
