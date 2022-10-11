b918d9e7f3fa1cad5b4a3b307cc59ee0a548c5d9
Claudio Toledo <claudiotoledoit@gmail.com>
2022-10-11 15:06:07 -0300

    remove old files

--- old-files/backend/database/db.js
+++
@@ -1,8 +0,0 @@
-//module.exports = {
-//  db: 'mongodb://localhost:27017/angular8mean'
-//};
-
-
-module.exports = {
-  db: 'mongodb+srv://root:globant@cluster0.dhfwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-};
